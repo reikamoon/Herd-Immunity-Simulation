e8e0d01ecd3cab38c1c496a48af172efa816eefd
squeaky1273 <pjriddell22@gmail.com>
2019-10-07 14:34:32 -0700

    put logged ends on separate lines

--- logger.py
+++ logger.py
@@ -23,7 +23,11 @@ class Logger(object):
         # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        metadata = (f"Population size ={pop_size}/nVaccine percentage = {vacc_percentage}/nVirus name = {virus_name}/nMortality Rate = {mortality_rate}/nRepoduction Rate = {basic_repro_num}/n")
+        metadata = (f"Population size ={pop_size}/n" + 
+        "Vaccine percentage = {vacc_percentage}/n" + 
+        "Virus name = {virus_name}/n" + 
+        "Mortality Rate = {mortality_rate}/n" + 
+        "Repoduction Rate = {basic_repro_num}/n")
         text_file = open(self.file_name, "w")
         text_file.append(self.file_name, "a")
         text_file.write(metadata)
