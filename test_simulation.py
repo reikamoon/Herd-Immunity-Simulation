cd3ca6e8a022bf66833f67cecee3a8e31fd7f4d4
squeaky1273 <pjriddell22@gmail.com>
2019-10-10 21:29:02 -0700

    added to sim test

--- test_simulation.py
+++ test_simulation.py
@@ -10,47 +10,48 @@ class Test(unittest.TestCase):
         virus = Virus("smallpox", 0.06, 0.15)
         sim = Simulation(100, .9, virus, 1)
    
-        assert sim.pop_size == 2500
-        assert sim.vacc_percentage == .99
-        assert sim.virus.name == "smallpox"
+        assert sim.pop_size == 100
+        assert sim.vacc_percentage == .90
         assert sim.initial_infected == 1
 
     def test_create_population(self):
         virus = Virus("small pox", 0.06, 0.15)
-        simulation = Simulation(2500, 0.99, virus, 1)
-        assert simulation.population == 100
-        assert simulation.virus.name == 'small pox'
-        assert simulation.total_infected == 1
-        assert simulation.current_infected == 1
-        assert simulation.total_dead == 0
-        assert simulation.file_name == 'log.txt'
-        assert len(simulation.newly_infected) is 0
+        sim = Simulation(2500, 0.99, virus, 1)
+        assert sim.population == 100
+        assert sim.total_infected == 1
+        assert sim.current_infected == 1
+        assert sim.total_dead == 0
+        assert sim.file_name == 'log.txt'
+        assert len(sim.newly_infected) is 0
 
     def test_simulation_should_continue(self):
         virus = Virus("small pox", 0.06, 0.15)
-        simulation = Simulation(2500, 0.90, virus, 1)
+        sim = Simulation(2500, 0.90, virus, 1)
         person1 = Person(1, True)
         person2 = Person(2, False)
-        assert simulation.simulation_should_continue() == True
+        assert sim._simulation_should_continue() == True
 
         person1.is_alive != True
         person2.is_alive != True
-        assert simulation.simulation_should_continue() == False
+        assert sim._simulation_should_continue() == False
 
     def test_interaction(self):
         virus = Virus("small pox", 0.06, 0.15)
-        simulation = Simulation(2500, 0.90, virus, 1)
+        sim = Simulation(2500, 0.90, virus, 1)
 
         person1 = Person(1, None)
         person2 = Person(2, virus)
 
-        simulation.interaction(person1, person2)
+        sim.interaction(person1, person2)
         assert person1.infection == virus
         assert person2.infection == None
 
+        assert person1.is_alive == True
+        assert person2.is_alive == True
+
     def test_infect_newly_infected(self):
         virus = Virus("small pox", 0.06, 0.15)
-        simulation = Simulation(2500, 0.90, virus, 1)
+        sim = Simulation(2500, 0.90, virus, 1)
 
-        assert simulation.population[1].infection == virus
-        assert simulation.population[0.9].infection == virus
+        assert sim.population[1].infection == virus
+        assert sim.population[0.9].infection == virus
